cc6fec4584d541ab88603aed8ca0a954b1d3a024
Chinayyds <1422554618@qq.com>
2021-08-18 16:25:05 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element—ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+
+// 引入axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    render: function(h) { return h(App) }
 }).$mount('#app')
